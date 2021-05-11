3fb9264636301ee1b70c35e5d228676af10635a7
johnmark14 <johmarkgabrentina@gmail.com>
2021-05-11 10:48:53 +0800

    Init

---
+++ src/main/AWTCounter.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author John Mark Gabrentina
+ *
+ */
+import java.awt.*;
+import java.awt.event.*;
+
+public class AWTCounter extends Frame {
+
+	private Label lblCount;
+	private TextField tfCount;
+	private Button btnCount;
+	private int count = 0;
+	
+	public AWTCounter() {
+		setLayout(new FlowLayout());
+		
+		lblCount = new Label("Counter");
+		add(lblCount);
+		
+		tfCount = new TextField(count + "", 10);
+		tfCount.setEditable(false);
+		add(tfCount);
+		
+		btnCount = new Button("Count");
+		add(btnCount);
+		
+		BtnCountListener listener = new BtnCountListener();
+		btnCount.addActionListener(listener);
+		
+		setTitle("AWT Counter");
+		setSize(300, 100);
+		
+		setVisible(true);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		AWTCounter app = new AWTCounter();
+
+	}
+	
+	private class BtnCountListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			++count;
+			
+			tfCount.setText(count + "");
+			
+		}
+		
+	}
+
+}
